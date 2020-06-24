10b5b7512c314563ef0822d6703ac52c35e210a3
zengyangzhou <43841091+zengyangzhou@users.noreply.github.com>
2020-06-24 15:47:41 +0800

    Update 不重复循环遍历.py

--- 不重复循环遍历.py
+++ 不重复循环遍历.py
@@ -6,3 +6,12 @@ for ind1,ele1 in enumerate(nums):
         for ind3,ele3 in enumerate(nums[ind2+ind1+2:]):
             #print(ind1,ind1+,ind3)
             print(ele1,ele2,ele3)
+            
+nums = ['a','b','c','d','e']
+for ind1,ele1 in enumerate(nums):
+
+    for ind2,ele2 in enumerate(nums[ind1+1:-2]):
+        for ind3,ele3 in enumerate(nums[ind2+ind1+2:-1]):
+            for ind4,ele4 in enumerate(nums[ind1+3+ind2+ind3:]):
+                #print(ind1,ind1+,ind3)
+                print(ele1,ele2,ele3,ele4)
